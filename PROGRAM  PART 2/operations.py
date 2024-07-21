23d0b8f4943a56e0967e913e6de5b1f5fc933f54
Prince <princeraj210180@gmail.com>
2024-07-21 16:14:34 +0530

    part 2

---
+++ PROGRAM  PART 2/operations.py
@@ -0,0 +1,14 @@
+# find armstrong number in interval -----------------P21
+lower = int(input("enter the lower number:"))
+upper = int(input("enter the upper number:"))
+
+for i in range(lower, upper+1):
+    order = len(str(i))
+    sum = 0
+    temp = i
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** order
+        temp //= 10
+        if num == sum:
+            print(i)
