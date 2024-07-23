276993fb1ea6bf64ce0203886bebdb092a4e8d17
Prince <princeraj210180@gmail.com>
2024-07-23 15:24:01 +0530

    some more programs

--- PROGRAM PART 3 (RECURSION)/programs.py
+++ PROGRAM PART 3 (RECURSION)/programs.py
@@ -45,3 +45,48 @@
 
 
 # convert binary to decial using recursion------------------P35
+# def ConvertBinary(n):
+#     if n > 1:
+#         ConvertBinary(n//2)
+#     print(n % 2, end="")
+
+
+# print("the binary of given number is :")
+# ConvertBinary(22)
+
+# program to add two matrices-----------------------P36
+# A = [[1, 5, 8],
+#      [4, 6, 7],
+#      [7, 2, 3]]
+# B = [[4, 5, 6],
+#      [8, 9, 1],
+#      [3, 5, 6]]
+# result = [[0, 0, 0],
+#           [0, 0, 0],
+#           [0, 0, 0]]
+# for i in range(len(A)):
+#     for j in range(len(A[0])):
+#         result[i][j] = A[i][j]+B[i][j]
+# for r in result:
+#     print(r)
+
+# program to transpose of matrics -----------------P37
+# A = [[1, 2, 3],
+#      [4, 5, 6]]
+# result = [[0, 0],
+#           [0, 0],
+#           [0, 0]]
+# for i in range(len(A)):
+#     for j in range(len(A[0])):
+#         result[j][i] = A[i][j]
+# for k in result:
+#     print(k)
+
+
+# check wheather a string is pelindrome or not -----------------P38
+word = input("enter a word here:")
+rev = word[::-1]  # it reverse the string
+if word == rev:
+    print("it is a pelindrome word.")
+else:
+    print("it is not pelindrome word.")
